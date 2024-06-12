098e09ae7d743ef7ac420d3a6c42e32d4705b124
First_Encounter <needenergy27@gmail.com>
2024-06-12 18:09:08 +0700

    added cards plan first step

--- src/components/BottomContent/BottomContent.jsx
+++ src/components/BottomContent/BottomContent.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "./BottomContent.css";
+
 const BottomContent = () => {
   return (
     <section className="middle-content">
@@ -9,9 +11,38 @@ const BottomContent = () => {
       <div className="plan-card-wrapper">
         <div className="beginer">
           <div className="plan-top">
-            <h3>Beginner</h3>
-            <img src="light.svg" alt="light" />
-            <span>Для небольшого исследования</span>
+            <div className="plan-title">
+              <h2>Beginner</h2>
+              <p>Для небольшого исследования</p>
+            </div>
+            <div className="plan-img">
+              <img src="beginer.svg" alt="light" />
+            </div>
+          </div>
+          <div className="plan-bottom">
+            <div className="price">
+              <h2>799 ₽</h2>
+              <span>1 200 ₽</span>
+            </div>
+            <p className="footnote">или 150 ₽/мес. при рассрочке на 24 мес.</p>
+            <p className="label-list">В тариф входит:</p>
+            <ul>
+              <li>
+                <div className="check"></div>
+                <p>Безлимитная история запросов</p>
+              </li>
+              <li>
+                <div className="check"></div>
+                <p>Безопасная сделка</p>
+              </li>
+              <li>
+                <div className="check"></div>
+                <p>Поддержка 24/7</p>
+              </li>
+            </ul>
+            <div className="button-plan">
+              <button className="request-button">Подробнее</button>
+            </div>
           </div>
         </div>
         <div className="pro"></div>
