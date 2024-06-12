d6167ae462df542d89bdc200f8c91ae0b8823240
First_Encounter <needenergy27@gmail.com>
2024-06-12 18:07:15 +0700

    added adaptive

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,31 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./Carousel.css";
 
 const Carousel = () => {
   const [index, setIndex] = useState(0);
+  const [numSlides, setNumSlides] = useState(1);
 
   const slides = [
     <div key="1" className="card-info">
       <img src="time_icon.png"></img>
       <p>
-        Высокая и оперативная скорость<br></br>
+        Высокая и оперативная скорость
         обработки заявки
       </p>
     </div>,
     <div key="2" className="card-info">
       <img src="search_icon.png"></img>
       <p>
-        Огромная комплексная база<br></br>
-        данных, обеспечивающая <br></br>
+        Огромная комплексная база
+        данных, обеспечивающая
         объективный ответ на запрос
       </p>
     </div>,
     <div key="3" className="card-info">
       <img src="security_icon.png"></img>
       <p>
-        Защита конфеденциальных сведений,<br></br>
-        не подлежащих разглашению по<br></br>
+        Защита конфеденциальных сведений,
+        не подлежащих разглашению по
         федеральному законодательству
       </p>
     </div>,
@@ -39,9 +40,26 @@ const Carousel = () => {
     setIndex((prevIndex) => (prevIndex - 1 + slides.length) % slides.length);
   };
 
-  const visibleSlides = slides.slice(index, index + 3);
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setNumSlides(3);
+      } else {
+        setNumSlides(1);
+    }
+    };
 
-  if (visibleSlides.length < 3) {
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const visibleSlides = slides.slice(index, index + numSlides);
+
+  if (visibleSlides.length < 3 && window.innerWidth > 768) {
     for (let i = 0; i <= 3 - visibleSlides.length; i++) {
       visibleSlides.push(slides[i]);
     }
