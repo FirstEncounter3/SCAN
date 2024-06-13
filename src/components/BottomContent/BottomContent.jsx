f18b66433a50fb97953e48f0ef7a5b3d33c92378
Test <Test@test.test>
2024-06-13 17:55:08 +0700

    external component separation

--- src/components/BottomContent/BottomContent.jsx
+++ src/components/BottomContent/BottomContent.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Rate from "../Rate/Rate";
+
 import "./BottomContent.css";
 
 const BottomContent = () => {
@@ -9,42 +11,8 @@ const BottomContent = () => {
         <h1>Наши тарифы</h1>
       </div>
       <div className="plan-card-wrapper">
-        <div className="beginer">
-          <div className="plan-top">
-            <div className="plan-title">
-              <h2>Beginner</h2>
-              <p>Для небольшого исследования</p>
-            </div>
-            <div className="plan-img">
-              <img src="beginer.svg" alt="light" />
-            </div>
-          </div>
-          <div className="plan-bottom">
-            <div className="price">
-              <h2>799 ₽</h2>
-              <span>1 200 ₽</span>
-            </div>
-            <p className="footnote">или 150 ₽/мес. при рассрочке на 24 мес.</p>
-            <p className="label-list">В тариф входит:</p>
-            <ul>
-              <li>
-                <div className="check"></div>
-                <p>Безлимитная история запросов</p>
-              </li>
-              <li>
-                <div className="check"></div>
-                <p>Безопасная сделка</p>
-              </li>
-              <li>
-                <div className="check"></div>
-                <p>Поддержка 24/7</p>
-              </li>
-            </ul>
-            <div className="button-plan">
-              <button className="request-button">Подробнее</button>
-            </div>
-          </div>
-        </div>
+        <Rate title="Beginner" subtitle="Для небольшого исследования" />
+        <Rate />
         <div className="pro"></div>
         <div className="business"></div>
       </div>
