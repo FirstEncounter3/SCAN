3aede4037fc88fcc9ec51ef864278f6284f1f6aa
Test <Test@test.test>
2024-06-13 17:53:26 +0700

    added constants for rates

---
+++ src/rates/rates.js
@@ -0,0 +1,12 @@
+const beginner = {
+    title: "Beginner",
+    subtitle: "Для небольшого исследования",
+    image: "beginner.svg",
+    smallImage: "beginner_small.svg",
+    price: "799 ₽",
+    oldPrice: "1 200 ₽",
+    footnote: "или 150 ₽/мес. при рассрочке на 24 мес.",
+    first: "Безлимитная история запросов",
+    second: "Безопасная сделка",
+    third: "Поддержка 24/7",
+};
