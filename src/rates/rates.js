330589627057c55e044f160b79cd7ed346cfe218
Test <Test@test.test>
2024-06-14 17:26:29 +0700

    fixed rates objects

--- src/rates/rates.js
+++ src/rates/rates.js
@@ -6,7 +6,35 @@ const beginner = {
     price: "799 ₽",
     oldPrice: "1 200 ₽",
     footnote: "или 150 ₽/мес. при рассрочке на 24 мес.",
-    first: "Безлимитная история запросов",
-    second: "Безопасная сделка",
-    third: "Поддержка 24/7",
+    firstAdvantage: "Безлимитная история запросов",
+    secondAdvantage: "Безопасная сделка",
+    thirdAdvantage: "Поддержка 24/7",
 };
+
+const pro = {
+    title: "Pro",
+    subtitle: "Для HR и фрилансеров",
+    image: "pro.svg",
+    smallImage: "pro_small.svg",
+    price: "1 299 ₽",
+    oldPrice: "2 600 ₽",
+    footnote: "или 279 ₽/мес. при рассрочке на 24 мес.",
+    firstAdvantage: "Все пункты тарифа Beginner",
+    secondAdvantage: "Экспорт истории",
+    thirdAdvantage: "Рекомендации по приоритетам",
+};
+
+const business = {
+    title: "Business",
+    subtitle: "Для корпоративных клиентов",
+    image: "business.svg",
+    smallImage: "business_small.svg",
+    price: "2 379 ₽",
+    oldPrice: "3 700 ₽",
+    footnote: "или 199 ₽/мес. при рассрочке на 24 мес.",
+    firstAdvantage: "Все пункты тарифа Pro",
+    secondAdvantage: "Безлимитное количество запросов",
+    thirdAdvantage: "Приоритетная поддержка",
+};
+
+export { beginner, pro, business };
