0f55fff39c37cf871fb856c62a783a3b0012cc74
First_Encounter <needenergy27@gmail.com>
2024-06-15 17:18:23 +0700

    fixed logic to render components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,32 +5,42 @@ import "./Header.css";
 
 import RegisterLoginPanel from "../RegisterLoginPanel/RegisterLoginPanel";
 import LogoutPanel from "../LogoutPanel/LogoutPanel";
+import RegisterLoginPanelMobile from "../RegisterLoginPanelMobile/RegisterLoginPanelMobile";
 
 import { useIsMobile } from "../../utils/utils";
 
 const Header = () => {
-  const [isLogin, setIsLogin] = React.useState(true);
+  const [isLogin, setIsLogin] = React.useState(false);
 
   return (
     <nav className="header">
       <div className="image-header-wrapper">
         <img src="header_left.png" alt="logo" />
       </div>
-      <div className="links-wrapper">
-        <Link to="/">Главная</Link>
-        <Link to="/about">Тарифы</Link>
-        <Link to="/contacts">FAQ</Link>
-      </div>
-      {/* НЕ СОВСЕМ РАБОЧИЙ ТАК, КАК ОЖИДАЕТСЯ, УЧАСТОК */}
-      {useIsMobile() ? (
-        isLogin? (
-          <LogoutPanel />
-         ) : ( 
-          <RegisterLoginPanel />
-         )
+      {useIsMobile() ? ( 
+          <RegisterLoginPanelMobile islogin={isLogin}/>
         ) : (
-          isLogin ? <div>mobileLogout</div> : <div>mobileLogin</div>
+          isLogin ? (
+            <>
+              <div className="links-wrapper">
+                <Link to="/">Главная</Link>
+                <Link to="/about">Тарифы</Link>
+                <Link to="/contacts">FAQ</Link>
+              </div>
+              <LogoutPanel />
+            </>
+          ) : (
+            <>
+              <div className="links-wrapper">
+                <Link to="/">Главная</Link>
+                <Link to="/about">Тарифы</Link>
+                <Link to="/contacts">FAQ</Link>
+              </div>
+              <RegisterLoginPanel />
+            </>
+          )
         )}
+      {}
     </nav>
   );
 };
