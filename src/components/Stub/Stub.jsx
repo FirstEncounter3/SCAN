d47f3ee2f93fa2ff0e5338b1c44208f23c32945a
First_Encounter <needenergy27@gmail.com>
2024-06-17 21:53:23 +0700

    add stub component for unusable buttons\links

---
+++ src/components/Stub/Stub.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Stub.css";
+
+const Stub = () => {
+  return (
+    <div className="stub-wrapper">
+      <h1>Данных пока нет</h1>
+    </div>
+  );
+};
+
+export default Stub;
