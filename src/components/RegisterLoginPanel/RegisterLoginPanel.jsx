b8194a00a4518e1d5c8d93e89402922c376bc689
First_Encounter <needenergy27@gmail.com>
2024-06-17 21:50:25 +0700

    added handleLoginCLick

--- src/components/RegisterLoginPanel/RegisterLoginPanel.jsx
+++ src/components/RegisterLoginPanel/RegisterLoginPanel.jsx
@@ -3,14 +3,18 @@ import { Link } from "react-router-dom";
 
 import "./RegisterLoginPanel.css";
 
+import { useLoginHandler } from "../../utils/utils";
 const RegisterLoginPanel = () => {
+
+  const { handleLoginClick } = useLoginHandler();
+
   return (
     <div className="buttons-wrapper">
       <Link to="/register" className="register-link">
         Зарегистрироваться
       </Link>
       <div className="vertical-line"></div>
-      <button className="login-button">Войти</button>
+      <button className="login-button" onClick={handleLoginClick}>Войти</button>
     </div>
   );
 };
