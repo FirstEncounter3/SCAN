a7b2d24af8504cd34ae50f3e431c439eef2a3faa
First_Encounter <needenergy27@gmail.com>
2024-06-17 21:46:38 +0700

    fixed routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,22 +5,23 @@ import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
 
+import Login from "../Login/Login";
+import Stub from "../Stub/Stub";
+
 import "./App.css"
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <Main />
-        <Footer />
-      </div>
-
+      <Header />
       <Routes>
-        <Route path="/"  />
-        <Route path="/about"/>
-        <Route path="/contacts" />
+        <Route path="/" element={<Main />} />
+        <Route path="/about" element={<Stub />} />
+        <Route path="/contacts" element={<Stub />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Stub />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
