2fed52d5969c89b1402c4ebf7ff5cf0608e3221f
First_Encounter <needenergy27@gmail.com>
2024-06-19 00:37:45 +0700

    add handle search button and develop mode check user authorize

--- src/components/TopContent/TopContent.jsx
+++ src/components/TopContent/TopContent.jsx
@@ -2,19 +2,31 @@ import React from "react";
 
 import "./TopContent.css";
 
+import { useSearchHandler } from "../../utils/utils";
+
 const TopContent = () => {
+  const [isLogin, setIsLogin] = React.useState(true);
+
+  const { handleSearchClick } = useSearchHandler();
+
   return (
     <section className="top-content">
       <div className="info-wrapper">
-        <h1>сервис по поиску <br>
-        </br>публикаций<br></br> 
-        о компании<br></br>
-         по его ИНН</h1>
+        <h1>
+          сервис по поиску <br></br>публикаций<br></br>о компании<br></br>
+          по его ИНН
+        </h1>
         <p>
-          Комплексный анализ публикаций, получение данных<br></br> 
-          в формате PDF на электронную почту.
+          Комплексный анализ публикаций, получение данных<br></br>в формате PDF
+          на электронную почту.
         </p>
-        <button className="request-button">Запросить данные</button>
+        <button
+          className="request-button"
+          style={{ display: isLogin ? "block" : "none" }}
+          onClick={handleSearchClick}
+        >
+          Запросить данные
+        </button>
       </div>
       <div className="img-wrapper">
         <img src="top_content_right.png" alt="top" />
