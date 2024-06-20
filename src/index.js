cd04419cc30f0b5c0bc25771102c877aaaae5143
First_Encounter <needenergy27@gmail.com>
2024-06-20 18:13:15 +0700

    add wrappers for redux and redux-persist

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import { createRoot } from "react-dom/client";
 import App from "./components/App/App";
+import { Provider } from "react-redux";
+import store from "./Store/store";
+
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
+let persistor = persistStore(store);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
