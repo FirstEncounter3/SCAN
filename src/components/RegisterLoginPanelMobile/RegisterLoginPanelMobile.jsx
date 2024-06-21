65d0b89e7f70babba5a217f33c019097c09486ff
First_Encounter <needenergy27@gmail.com>
2024-06-21 18:52:48 +0700

    add logout handler

--- src/components/RegisterLoginPanelMobile/RegisterLoginPanelMobile.jsx
+++ src/components/RegisterLoginPanelMobile/RegisterLoginPanelMobile.jsx
@@ -4,9 +4,12 @@ import { Link } from "react-router-dom";
 import "./RegisterLoginPanelMobile.css";
 
 import { useLoginHandler } from "../../utils/utils";
+import { logout } from "../../Store/userSlice";
+import { useDispatch } from "react-redux";
 
 const RegisterLoginPanelMobile = ({ islogin }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const dispatch = useDispatch();
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -17,7 +20,12 @@ const RegisterLoginPanelMobile = ({ islogin }) => {
   const handleLoginClickAndCloseMenu = () => {
     handleLoginClick();
     setIsOpen(false);
-  }
+  };
+
+  const handleLogoutClickAndCloseMenu = () => {
+    dispatch(logout());
+    setIsOpen(false);
+  };
 
   return (
     <div className="burger-menu">
@@ -34,16 +42,24 @@ const RegisterLoginPanelMobile = ({ islogin }) => {
           <img src="header_left_transparent.png" alt="logo" />
         </div>
         <div className="menu-links">
-          <Link to="/" onClick={toggleMenu}>Главная</Link>
-          <Link to="/contacts" onClick={toggleMenu}>Тарифы</Link>
-          <Link to="/about" onClick={toggleMenu}>FAQ</Link>
+          <Link to="/" onClick={toggleMenu}>
+            Главная
+          </Link>
+          <Link to="/contacts" onClick={toggleMenu}>
+            Тарифы
+          </Link>
+          <Link to="/about" onClick={toggleMenu}>
+            FAQ
+          </Link>
         </div>
         <div className="menu-footer">
           {islogin ? (
-            <button>Выйти</button>
+            <button onClick={handleLogoutClickAndCloseMenu}>Выйти</button>
           ) : (
             <>
-              <Link to="/register" onClick={toggleMenu}>Зарегистрироваться</Link>
+              <Link to="/register" onClick={toggleMenu}>
+                Зарегистрироваться
+              </Link>
               <button onClick={handleLoginClickAndCloseMenu}>Войти</button>
             </>
           )}
