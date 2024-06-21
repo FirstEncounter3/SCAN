88828c92e6d16ae987af600f3fc251bde90f8e3a
First_Encounter <needenergy27@gmail.com>
2024-06-21 18:46:13 +0700

    add loader while request in process

---
+++ src/components/UserInfoBoardLoaling/UserInfoBoardLoading.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./UserInfoBoardLoading.css";
+
+function UserInfoBoardLoading() {
+  return (
+    <div className="user-info-wrapper-loader">
+        <span class="loader"></span>
+    </div>
+  );
+}
+
+export default UserInfoBoardLoading;
