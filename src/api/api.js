ea61693e3feb8bb821601e657045555fa1ef8652
First_Encounter <needenergy27@gmail.com>
2024-06-22 17:29:38 +0700

    add getHistograms method

--- src/api/api.js
+++ src/api/api.js
@@ -20,3 +20,72 @@ export const getUserInfo = async (accessToken) =>  {
     console.log(request.data)
     return request.data
 }
+
+export const getHistograms = async (accessToken) =>  {
+    const url = `${baseUrl}/api/v1/objectsearch/histograms`
+    const data = {
+        "issueDateInterval": {
+          "startDate": "2023-01-01T00:00:00+03:00",
+          "endDate": "2024-08-31T23:59:59+03:00"
+        },
+        "searchContext": {
+          "targetSearchEntitiesContext": {
+            "targetSearchEntities": [
+              {
+                "type": "company",
+                "sparkId": null,
+                "entityId": null,
+                "inn": 7710137066,
+                "maxFullness": true,
+                "inBusinessNews": null
+              }
+            ],
+            "onlyMainRole": true,
+            "tonality": "any",
+            "onlyWithRiskFactors": false,
+            "riskFactors": {
+              "and": [],
+              "or": [],
+              "not": []
+            },
+            "themes": {
+              "and": [],
+              "or": [],
+              "not": []
+            }
+          },
+          "themesFilter": {
+            "and": [],
+            "or": [],
+            "not": []
+          }
+        },
+        "searchArea": {
+          "includedSources": [],
+          "excludedSources": [],
+          "includedSourceGroups": [],
+          "excludedSourceGroups": []
+        },
+        "attributeFilters": {
+          "excludeTechNews": true,
+          "excludeAnnouncements": true,
+          "excludeDigests": true
+        },
+        "similarMode": "duplicates",
+        "limit": 1000,
+        "sortType": "sourceInfluence",
+        "sortDirectionType": "desc",
+        "intervalType": "month",
+        "histogramTypes": [
+          "totalDocuments",
+          "riskFactors"
+        ]
+      }
+
+    const request = await axios.post(url, data, {
+        headers: {
+            "Authorization": `Bearer ${accessToken}`
+        }
+    });
+    return request.data 
+}
