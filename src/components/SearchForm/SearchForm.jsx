bf805937d2029e496457936805927bbad421db0d
First_Encounter <needenergy27@gmail.com>
2024-06-25 00:19:23 +0700

    first steps to get data form child and send in other component

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -14,25 +14,50 @@ import { getHistograms } from "../../api/api";
 const SearchForm = () => {
   const isMobile = useIsMobile();
   const accessToken = useSelector((state) => state.user.accessToken);
-  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [inn, setInn] = useState("");
+  const [documentsCount, setDocumentsCount] = useState("");
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  const handleInnChange = (innValue) => {
+    setInn(innValue);
+    setIsFormValid(inn !== "" && documentsCount > "");
+  };
+
+  const handleDocumentCountChange = (documentCountValue) => {
+    setDocumentsCount(documentCountValue);
+    setIsFormValid(inn !== "" && documentsCount > "");
+  };
 
   const makeSearchRequest = async () => {
     setIsLoading(true);
     const response = await getHistograms(accessToken);
     console.log(response);
     setIsLoading(false);
-    navigate("/results");
+
+    const params = new URLSearchParams();
+    params.append("inn", inn);
+    params.append("documentsCount", documentsCount);
+
+    navigate(`/results?${params.toString()}`);
   };
 
   return (
     <div className="search-form-wrapper">
       <div className="search-from-inputs-and-checkboxes-wrapper">
         {isMobile ? (
-          <SearchInputs />
+          <SearchInputs
+            onInnChange={handleInnChange}
+            onDocumentCountChange={handleDocumentCountChange}
+          />
         ) : (
           <>
-            <SearchInputs />
+            <SearchInputs
+              onInnChange={handleInnChange}
+              onDocumentCountChange={handleDocumentCountChange}
+            />
             <SearchCheckboxes />
           </>
         )}
@@ -40,9 +65,10 @@ const SearchForm = () => {
       <div className="search-form-date-and-submit-wrapper">
         <DateRangePicker />
         <div className="search-button-wrapper">
-          <button className="search-button" 
-          onClick={makeSearchRequest}
-          disabled={isLoading ? true : false}
+          <button
+            className={`search-button ${isFormValid ? "" : "inactive"}`}
+            onClick={makeSearchRequest}
+            disabled={isLoading || !isFormValid}
           >
             {isLoading ? "Получение данных..." : "Поиск"}
           </button>
