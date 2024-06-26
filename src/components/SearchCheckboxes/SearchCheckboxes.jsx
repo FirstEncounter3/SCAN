54eaaefea9dfa96c5d7835f652533c171c9c825c
First_Encounter <needenergy27@gmail.com>
2024-06-26 19:01:23 +0700

    collect all state and ids for checkboxes

--- src/components/SearchCheckboxes/SearchCheckboxes.jsx
+++ src/components/SearchCheckboxes/SearchCheckboxes.jsx
@@ -2,35 +2,43 @@ import React from "react";
 
 import "./SearchCheckboxes.css";
 
-const SearchCheckboxes = () => {
+const SearchCheckboxes = ({ onCheckboxChange }) => {
+  const handleCheckboxChange = (e) => {
+    const checkboxId = e.target.id
+    const isChecked = e.target.checked;
+
+    onCheckboxChange(checkboxId, isChecked)
+  }
+
+
   return (
     <div className="checkbox-wrapper">
       <div className="checkbox-and-label">
-        <input type="checkbox" id="completeness"></input>
+        <input type="checkbox" id="completeness" onChange={handleCheckboxChange}></input>
         <label htmlFor="completeness">Признак максимальной полноты</label>
       </div>
       <div className="checkbox-and-label">
-        <input type="checkbox" id="business-context"></input>
+        <input type="checkbox" id="business-context" onChange={handleCheckboxChange}></input>
         <label htmlFor="business-context">Упоминания в бизнес-контексте</label>
       </div>
       <div className="checkbox-and-label">
-        <input type="checkbox" id="main-role"></input>
+        <input type="checkbox" id="main-role" onChange={handleCheckboxChange}></input>
         <label htmlFor="main-role">Главная роль в публикации</label>
       </div>
       <div className="checkbox-and-label">
-        <input type="checkbox" id="risk-factor"></input>
+        <input type="checkbox" id="risk-factor" onChange={handleCheckboxChange}></input>
         <label htmlFor="risk-factor">Публикации только с риск-факторами</label>
       </div>
       <div className="checkbox-and-label">
-        <input type="checkbox" id="technical-news"></input>
+        <input type="checkbox" id="technical-news" onChange={handleCheckboxChange}></input>
         <label htmlFor="technical-news">Включать технические новости рынков</label>
       </div>
       <div className="checkbox-and-label">
-        <input type="checkbox" id="announcements"></input>
+        <input type="checkbox" id="announcements" onChange={handleCheckboxChange}></input>
         <label htmlFor="announcements">Включать анонсы и календари</label>
       </div>
       <div className="checkbox-and-label">
-        <input type="checkbox" id="summaries"></input>
+        <input type="checkbox" id="summaries" onChange={handleCheckboxChange}></input>
         <label htmlFor="summaries">Включать сводки новостей</label>
       </div>
     </div>
