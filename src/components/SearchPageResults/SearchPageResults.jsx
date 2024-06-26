5b2b6ef59cf05e0191db626e4a681e7704d5ce4e
First_Encounter <needenergy27@gmail.com>
2024-06-26 19:02:22 +0700

    add getting query vars from url

--- src/components/SearchPageResults/SearchPageResults.jsx
+++ src/components/SearchPageResults/SearchPageResults.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 
 import SearchPageResultsTopContent from "../SearchPageResultsTopContent/SearchPageResultsTopContent";
 import ResultsSummary from "../ResultsSummary/ResultsSummary";
@@ -8,6 +9,13 @@ import Unathorized from "../Unathorized/Unathorized";
 import "./SearchPageResults.css";
 
 const SearchPageResults = () => {
+  const location = useLocation();
+  const search = location.search;
+  const query = new URLSearchParams(search);
+
+  console.log(query);
+
+
   const isLogin = useSelector((state) => state.user.isLogin);
   return isLogin ? (
     <div className="search-page-results-wrapper">
