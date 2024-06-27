f7b2580706010d3272416c034b59f2dda451e9ff
First_Encounter <needenergy27@gmail.com>
2024-06-28 02:43:16 +0700

    pass props

--- src/components/SearchPageResults/SearchPageResults.jsx
+++ src/components/SearchPageResults/SearchPageResults.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 
 import SearchPageResultsTopContent from "../SearchPageResultsTopContent/SearchPageResultsTopContent";
 import ResultsSummary from "../ResultsSummary/ResultsSummary";
+import ResultsBottomContent from "../ResultsBottomContent/ResultsBottomContent";
 import Unathorized from "../Unathorized/Unathorized";
 
 import "./SearchPageResults.css";
 
+import { getQueryParams } from "../../utils/utils";
+
 const SearchPageResults = () => {
   const isLogin = useSelector((state) => state.user.isLogin);
+  const accessToken = useSelector((state) => state.user.accessToken);
+  const location = useLocation();
+  const queryParams = getQueryParams(location);
+  
   return isLogin ? (
     <div className="search-page-results-wrapper">
       <SearchPageResultsTopContent />
-      <ResultsSummary />
+      <ResultsSummary queryParams={queryParams} accessToken={accessToken} />
+      <ResultsBottomContent queryParams={queryParams} accessToken={accessToken}/>
     </div>
   ) : (
     <Unathorized />
