6394b9f435ae9bb89ae23bfde06c8eb51b060057
First_Encounter <needenergy27@gmail.com>
2024-06-27 19:10:33 +0700

    remove api call

--- src/components/SearchPageResults/SearchPageResults.jsx
+++ src/components/SearchPageResults/SearchPageResults.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
-import { useLocation } from "react-router-dom";
 
 import SearchPageResultsTopContent from "../SearchPageResultsTopContent/SearchPageResultsTopContent";
 import ResultsSummary from "../ResultsSummary/ResultsSummary";
@@ -8,39 +7,7 @@ import Unathorized from "../Unathorized/Unathorized";
 
 import "./SearchPageResults.css";
 
-import { getHistograms } from "../../api/api";
-
 const SearchPageResults = () => {
-  const location = useLocation();
-  const search = location.search;
-  const query = new URLSearchParams(search);
-
-  const startDate = query.get("startDate");
-  const endDate = query.get("endDate");
-  const documentsCount = query.get("documentsCount");
-  const inn = query.get("inn");
-  const tonality = query.get("tonality");
-  const mainRole = query.get("mainRole");
-  const riskFactors = query.get("riskFactors");
-
-  const accessToken = useSelector((state) => state.user.accessToken);
-
-  console.log(query);
-  console.log(query.get('startDate')); //25.06.2024T00:00:00+03:00
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await getHistograms(accessToken, startDate, endDate, documentsCount, inn, tonality, mainRole, riskFactors);
-        console.log(response);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData(); // {"data":[{"data":[{"date":"2024-06-01T03:00:00+03:00","value":8398}],"histogramType":"totalDocuments"},{"data":[{"date":"2024-06-01T03:00:00+03:00","value":151}],"histogramType":"riskFactors"}]}
-  }, [accessToken]);
-
   const isLogin = useSelector((state) => state.user.isLogin);
   return isLogin ? (
     <div className="search-page-results-wrapper">
