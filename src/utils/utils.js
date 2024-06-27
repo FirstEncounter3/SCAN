f9f7d11b5a19ae5cbd49ab40fef0065fa2b88e11
First_Encounter <needenergy27@gmail.com>
2024-06-28 02:40:36 +0700

    add func to get query params for reuse

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -46,3 +46,36 @@ export const useSearchResultsHandler = () => {
 
   return { handleSearchResultsClick };
 };
+
+export const getQueryParams = (location) => {
+  const search = location.search;
+  const query = new URLSearchParams(search);
+
+  const startDate = query.get("startDate");
+  const endDate = query.get("endDate");
+  const documentsCount = query.get("documentsCount");
+  const inn = query.get("inn");
+  const tonality = query.get("tonality");
+  const mainRole = query.get("mainRole");
+  const riskFactors = query.get("riskFactors");
+  const completeness = query.get("completeness");
+  const businessContext = query.get("businessContext");
+  const technicalNews = query.get("technicalNews");
+  const announcements = query.get("announcements");
+  const summaries = query.get("summaries");
+
+  return {
+    startDate,
+    endDate,
+    documentsCount,
+    inn,
+    tonality,
+    mainRole,
+    riskFactors,
+    completeness,
+    businessContext,
+    technicalNews,
+    announcements,
+    summaries,
+  };
+};
