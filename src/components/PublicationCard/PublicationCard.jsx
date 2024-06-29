87ad09dc6b945bb6405cbf60d3497d323025cf9d
First_Encounter <needenergy27@gmail.com>
2024-06-30 02:40:25 +0700

    output data from props

--- src/components/PublicationCard/PublicationCard.jsx
+++ src/components/PublicationCard/PublicationCard.jsx
@@ -1,36 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
+import { parseXML } from "../../utils/utils";
 
-function PublicationCard({ accessToken, encodedIds }) {
+import "./PublicationCard.css";
+
+function PublicationCard({ attributes, content, issueDate, title, source, url }) {
+  const parsedXml = parseXML(content.markup);
+  const parsedDate = issueDate.split('T')[0].split('-').reverse().join('.');
+  const text = Array.isArray(parsedXml.scandoc.sentence)
+  ? parsedXml.scandoc.sentence.map(sentence => sentence._text).join(" ") : "";
+
   return (
     <section className="publication-card-wrapper">
       <div className="publication-date-and-source">
-        <span>13.09.2021</span>
-        <a href="#">Комсомольская правда</a>
+        <span>{parsedDate}</span>
+        <Link to={url}>{source.name}</Link>
       </div>
       <div className="publication-header-and-bage">
-        <h3>Скиллфэктори - лучшая онлайн-школа для будущих айтишников</h3>
+        <h2>{title.text}</h2>
         <span>Технические новости</span>
       </div>
       <div className="publication-img-content">
-        <img src="publication.png" alt="publication" />
+        <img src={''} alt="publication" />
       </div>
       <div className="publication-text-content">
         <p>
-          SkillFactory — школа для всех, кто хочет изменить свою карьеру и
-          жизнь. С 2016 года обучение прошли 20 000+ человек из 40 стран с 4
-          континентов, самому взрослому студенту сейчас 86 лет. Выпускники
-          работают в Сбере, Cisco, Bayer, Nvidia, МТС, Ростелекоме, Mail.ru,
-          Яндексе, Ozon и других топовых компаниях. Принципы SkillFactory:
-          акцент на практике, забота о студентах и ориентир на трудоустройство.
-          80% обучения — выполнение упражнений и реальных проектов. Каждого
-          студента поддерживают менторы, 2 саппорт-линии и комьюнити курса. А
-          карьерный центр помогает составить резюме, подготовиться к
-          собеседованиям и познакомиться с IT-рекрутерами.
+          {text}
         </p>
       </div>
       <div className="publication-footer">
-        <button>Читать в источнике</button>
-        <span>2 543 слова</span>
+        <Link to={url}>Читать в источнике</Link>
+        <span>{attributes.wordCount} слова</span>
       </div>
     </section>
   );
