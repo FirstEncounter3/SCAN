391fef4192736b8b266bbc8c7902feaac50c1f93
Test <Test@test.test>
2024-07-01 17:18:58 +0700

    add fromatText func, concatenateText from xml, tryFindImg, badge switch cases

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -85,3 +85,95 @@ export const parseXML = (xml) => {
   const json = xml2js(xml, { compact: true });
   return json;
 };
+
+export const smallFormattedText = (text) => {
+  const formattedText = text
+    .replace(/<[^>]*>?/gm, '')
+    .replace(/[\s\n]+/g, ' ')
+    .replace(/[\u00A0\u2000-\u200B\u2028-\u202F\u205F\u3000]/g, ' ')
+    .trim()
+    .slice(0, 500);
+  return formattedText;
+};
+
+export const concatenateXmlText = (xmlString) => {
+  const textParser = new DOMParser();
+  const xmlDoc = textParser.parseFromString(xmlString, "text/xml");
+
+  // Получаем корневой элемент XML-документа
+  const rootElement = xmlDoc.documentElement;
+
+  // Создаем пустую строку для хранения текста
+  let text = "";
+
+  // Идем по всем дочерним элементам корневого элемента
+  const childNodes = rootElement.childNodes;
+  for (let i = 0; i < childNodes.length; i++) {
+    const childNode = childNodes[i];
+
+    // Если элемент является текстовым узлом, добавляем его текст к нашей строке
+    if (childNode.nodeType === Node.TEXT_NODE) {
+      text += childNode.textContent;
+    }
+
+    // Если элемент имеет дочерние элементы, рекурсивно обрабатываем их
+    else if (childNode.hasChildNodes()) {
+      text += processChildNodes(childNode);
+    }
+  }
+
+  function processChildNodes(node) {
+    let text = "";
+
+    // Идем по всем дочерним элементам текущего узла
+    const childNodes = node.childNodes;
+    for (let i = 0; i < childNodes.length; i++) {
+      const childNode = childNodes[i];
+
+      // Если элемент является текстовым узлом, добавляем его текст к нашей строке
+      if (childNode.nodeType === Node.TEXT_NODE) {
+        text += childNode.textContent;
+      }
+
+      // Если элемент имеет дочерние элементы, рекурсивно обрабатываем их
+      else if (childNode.hasChildNodes()) {
+        text += processChildNodes(childNode);
+      }
+    }
+
+    return text;
+  }
+
+  return text;
+};
+
+export const tryFindImg = (text) => {
+  let imgUrl = null;
+  const imgSearch = text.match(/img src=["'](.*?)["']/g);
+  if (imgSearch) {
+    imgUrl = imgSearch[0].split("=")[1].replace(/^"|"$/g, "");
+  } else {
+    imgUrl = false;
+  }
+  return imgUrl
+};
+
+export const badge = (tech, announcement, digest) => {
+  let badgeText = ''
+  switch(true) {
+    case tech:
+      badgeText = "Технические новости"
+      break;
+
+    case announcement:
+      badgeText = "Анонсы и события"
+      break;
+    
+    case digest:
+      badgeText = "Сводки новостей"
+      break;
+    default:
+      badgeText = "Новости"
+  }
+  return badgeText
+};
